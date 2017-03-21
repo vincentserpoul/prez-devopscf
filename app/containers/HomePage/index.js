8c547290969739c4462f17a2df3ee625bc923797
Vincent Serpoul <vincent@serpoul.com>
2017-03-21 19:35:45 +0800

    add cloud overview

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -10,15 +10,12 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-import messages from './messages';
+import Presentation from 'components/Presentation';
 
 export default class HomePage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <Presentation />
     );
   }
 }
